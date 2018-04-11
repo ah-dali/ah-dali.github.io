2c6385cd5f61fa962a30e8a1850279301d0d0f7e
ah-dali <38163951+ah-dali@users.noreply.github.com>
2018-04-11 07:34:55 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function switchImage() {
     var img = document.getElementById("image");
-    img.src = this.value;
+    img.src = "https://github.com/ah-dali/ah-dali.github.io/edit/master/jpg";
     return false;
 }
 document.getElementById("pickArtist").onchange = setImage;
