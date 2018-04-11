44b219e530875f8f88b5a93a589966a2383e02a8
ah-dali <38163951+ah-dali@users.noreply.github.com>
2018-04-11 08:31:30 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function switchImage() {
     var img = document.getElementById("image");
     img.src = "https://github.com/ah-dali/ah-dali.github.io/tree/master/jpg";
-    return false;
+    return true;
 }
 document.getElementById("pickArtist").onchange = switchImage;
