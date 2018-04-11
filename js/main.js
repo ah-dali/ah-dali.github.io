bd7ee25ca5478974818bc17b30ab90c1e61336ac
ah-dali <38163951+ah-dali@users.noreply.github.com>
2018-04-11 07:15:13 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-var imageList = Array();
-for (var i = 1; i <= 10; i++) {
-    imageList[i] = new Image(70, 70);
-    imageList[i].src = "https://github.com/ah-dali/ah-dali.github.io/tree/master/jpg";
-}
-
 function switchImage() {
-    var selectedImage = document.myForm.switch.options[document.myForm.switch.selectedIndex].value;
-    document.myImage.src = imageList[selectedImage].src;
+    var img = document.getElementById("image");
+    img.src = this.value;
+    return false;
 }
+document.getElementById("pickArtist").onchange = setImage;
